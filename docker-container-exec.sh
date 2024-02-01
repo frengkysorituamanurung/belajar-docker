8f69e85046369b85bd0620a2c870683a384c0436
Frengky Manurung <frengkykozeks@gmail.com>
2024-02-01 21:23:15 +0700

    update command

---
+++ docker-container-exec.sh
@@ -0,0 +1,3 @@
+docker container exec -i -t namacontainer /bin/bash #masuk kedalam container mencoba mengeksekusi program bash script yang terdapat didalam container dengan bantuan container exec
+-i #argumen interaktif, menjaga input tetap aktif
+-t #argumen untuk terminal akses
---
+++ docker-container-logs.sh
@@ -0,0 +1,4 @@
+docker container logs namacontainer #melihat log aplikasi di container
+
+docker container logs -f namacontainer #melihat log aplikasi di container secara realtime
+
