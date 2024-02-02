675b082e21fc21fedef3392348337013928ea47d
Frengky Manurung <frengkykozeks@gmail.com>
2024-02-02 13:48:15 +0700

    edit

--- docker-container-exec.sh
+++ docker-container-exec.sh
@@ -1,3 +1,5 @@
 docker container exec -i -t namacontainer /bin/bash #masuk kedalam container mencoba mengeksekusi program bash script yang terdapat didalam container dengan bantuan container exec
 -i #argumen interaktif, menjaga input tetap aktif
 -t #argumen untuk terminal akses
+
+docker exec -t namacontainer //bin/bash
