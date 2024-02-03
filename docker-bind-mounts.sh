4625f2182c28433ce07207a9f3c6f2857e575648
Frengky Manurung <frengkykozeks@gmail.com>
2024-02-03 15:01:34 +0700

    ctt mongo

---
+++ docker-bind-mounts.sh
@@ -0,0 +1,1 @@
+docker container create --name mongodata --publish 27018:27017 --mount "type=bind,source=/d/My Porto/docker/hello-docker/mongo-data,destination=/data/db" --env MONGO_INITDB_ROOT_USERNAME=frengky --env MONGO_INITDB_ROOT_PASSWORD=frengky mongo:latest
