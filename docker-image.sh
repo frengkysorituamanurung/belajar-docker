4160ad19f505404c8b9034d9f809cfe8237a7801
Frengky Manurung <frengkykozeks@gmail.com>
2024-02-26 10:17:00 +0700

    fix bugs

--- docker-image.sh
+++ docker-image.sh
@@ -3,3 +3,5 @@ docker image ls #untuk melihat docker image yang terdapat di dalam docker daemon
 docker image pull redis:latest #downloaded docker image
 
 docker image rm:alpine:latest #menghapus docker image
+
+docekr image -it namaimage
